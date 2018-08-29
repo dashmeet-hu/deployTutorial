804e951ae04d19875ff1780f536ad44add35195c
Dashmeet Singh <dashmeet@happilyunmarried.com>
2018-08-29 15:30:46 +0530

    sample bash script

---
+++ scripts/start_node_application.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cat ../appspec.yml
+rm -rf ../code/removethisfile.html
---
+++ scripts/stop_node_application.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# stop node application
+#b=`npm list -g | grep pm2`;
+
+# if [ "`npm list -g pm2`" ]
+# then echo 'install pm2'
+# else echo 'pm2 is already installed'
+# 	fi
+
+# if 
+echo "stopping node application"
+
