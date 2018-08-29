9a9ba8a1f5fe52b0d59849860c6c600bf5a3bdbe
Dashmeet Singh <35026920+dashmeet-hu@users.noreply.github.com>
2018-08-29 16:38:39 +0530

    absolute paths for files

--- scripts/start_node_application.sh
+++ scripts/start_node_application.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-cat ../appspec.yml
-rm -rf ../code/removethisfile.html
-mkdir dsb-test-directory
-cd dsb-test-directory
+cat /var/www/html/cd/appspec.yml
+rm -rf /var/www/html/cd/code/removethisfile.html
+mkdir /var/www/html/cd/dsb-test-directory
+cd /var/www/html/cd/dsb-test-directory
 touch newFile.js
